842d166df679528651c4710ab6dc873d9d185810
ben-congram <ben.congram@robotics.utias.utoronto.ca>
2021-07-08 17:14:03 -0400

    Refactor plot_localization.py
    Also fix missing T_sv fields in scripts

--- main/src/vtr_testing_stereo/scripts/plot_localization.py
+++ main/src/vtr_testing_stereo/scripts/plot_localization.py
@@ -14,9 +14,21 @@ matplotlib.use("TkAgg")  # Can change to 'Agg' for non-interactive mode
 matplotlib.rcParams["pdf.fonttype"] = 42
 matplotlib.rcParams["ps.fonttype"] = 42
 
+# set sizes
+SMALL_SIZE = 10
+MEDIUM_SIZE = 12
+BIGGER_SIZE = 16
+plt.rc("font", size=MEDIUM_SIZE)  # controls default text sizes
+plt.rc("axes", titlesize=MEDIUM_SIZE)  # fontsize of the axes title
+plt.rc("axes", labelsize=SMALL_SIZE)  # fontsize of the x and y labels
+plt.rc("xtick", labelsize=MEDIUM_SIZE)  # fontsize of the tick labels
+plt.rc("ytick", labelsize=MEDIUM_SIZE)  # fontsize of the tick labels
+plt.rc("figure", titlesize=MEDIUM_SIZE)  # fontsize of the figure title
+plt.rc("legend", fontsize=SMALL_SIZE)  # legend fontsize
 
-def read_vo(vo_dir):
-    with open(osp.join(vo_dir, "vo.csv"), newline='') as resultfile:
+
+def read_vo(vo_dir, vo_file="vo.csv"):
+    with open(osp.join(vo_dir, vo_file), newline='') as resultfile:
         spamreader = csv.reader(resultfile, delimiter=',', quotechar='|')
         tmp = []
         for i, row in enumerate(spamreader):
@@ -28,11 +40,11 @@ def read_vo(vo_dir):
     return np.array(tmp)
 
 
-def read_vo_transforms(data_dir):
+def read_vo_transforms(data_dir, vo_file="vo.csv"):
     """Extract integrated VO transformations from teach run. Used to transform loc results"""
     vo_transforms = {}
 
-    with open(osp.join(data_dir, "vo.csv"), newline='') as resultfile:
+    with open(osp.join(data_dir, vo_file), newline='') as resultfile:
         spamreader = csv.reader(resultfile, delimiter=',', quotechar='|')
 
         for i, row in enumerate(spamreader):
@@ -46,12 +58,12 @@ def read_vo_transforms(data_dir):
     return vo_transforms
 
 
-def read_loc(teach_dir, repeat_dir):
+def read_loc(teach_dir, repeat_dir, loc_file="loc.csv", teach_vo_file="vo.csv"):
     """Robot position from composing integrated VO and localization"""
 
-    vo_transforms = read_vo_transforms(teach_dir)
+    vo_transforms = read_vo_transforms(teach_dir, teach_vo_file)
 
-    with open(osp.join(repeat_dir, "loc.csv"), newline='') as resultfile:
+    with open(osp.join(repeat_dir, loc_file), newline='') as resultfile:
         spamreader = csv.reader(resultfile, delimiter=',', quotechar='|')
         r_world_set = np.empty((0, 4))
         r_qm_in_m_set = np.empty((0, 4))
@@ -73,12 +85,12 @@ def read_loc(teach_dir, repeat_dir):
     return r_world_set, r_qm_in_m_set
 
 
-def read_loc_sensor(teach_dir, repeat_dir, T_sv):
-    """Returns positional localization estimate in the sensor frame"""
+def read_loc_sensor(teach_dir, repeat_dir, T_sv, loc_file="loc.csv", teach_vo_file="vo.csv"):
+    """Returns positional localization estimate in the (moving) sensor frame"""
 
-    teach_data = np.genfromtxt(osp.join(teach_dir, "vo.csv"), delimiter=',', skip_header=1)
+    teach_data = np.genfromtxt(osp.join(teach_dir, teach_vo_file), delimiter=',', skip_header=1)
 
-    with open(osp.join(repeat_dir, "loc.csv"), newline='') as resultfile:
+    with open(osp.join(repeat_dir, loc_file), newline='') as resultfile:
         spamreader = csv.reader(resultfile, delimiter=',', quotechar='|')
 
         r_qsms_in_ms_set = np.empty((0, 9))
@@ -94,12 +106,12 @@ def read_loc_sensor(teach_dir, repeat_dir, T_sv):
                 teach_time_idx = np.argmax(teach_data[:, 2] == float(row[4]))  # find time corresponding to map vertex
                 r_qs_ms[0] = teach_data[teach_time_idx, 0]
                 r_qs_ms[1] = float(row[0])   # repeat timestamp
-                r_qs_ms[2] = T_ms_qs[0, 3]   # x
-                r_qs_ms[3] = T_ms_qs[1, 3]   # y
-                r_qs_ms[4] = T_ms_qs[2, 3]   # z
-                r_qs_ms[5] = 0   # x gt (TBD)
-                r_qs_ms[6] = 0   # y gt (TBD)
-                r_qs_ms[7] = 0   # z gt (TBD)
+                r_qs_ms[2] = T_ms_qs[0, 3]   # x estimated
+                r_qs_ms[3] = T_ms_qs[1, 3]   # y estimated
+                r_qs_ms[4] = T_ms_qs[2, 3]   # z estimated
+                r_qs_ms[5] = 0   # x ground truth (TBD)
+                r_qs_ms[6] = 0   # y ground truth (TBD)
+                r_qs_ms[7] = 0   # z ground truth (TBD)
                 r_qs_ms[8] = 0   # cumulative distance travelled based on teach ground truth (TBD)
 
                 r_qsms_in_ms_set = np.append(r_qsms_in_ms_set, r_qs_ms.T, axis=0)
@@ -153,68 +165,44 @@ def interpolate_and_rotate(gt_repeat, gt_teach, r_loc_in_gps_frame):
 
 def main():
 
-    # set sizes
-    SMALL_SIZE = 10
-    MEDIUM_SIZE = 12
-    BIGGER_SIZE = 16
-    plt.rc("font", size=MEDIUM_SIZE)  # controls default text sizes
-    plt.rc("axes", titlesize=MEDIUM_SIZE)  # fontsize of the axes title
-    plt.rc("axes", labelsize=SMALL_SIZE)  # fontsize of the x and y labels
-    plt.rc("xtick", labelsize=MEDIUM_SIZE)  # fontsize of the tick labels
-    plt.rc("ytick", labelsize=MEDIUM_SIZE)  # fontsize of the tick labels
-    plt.rc("figure", titlesize=MEDIUM_SIZE)  # fontsize of the figure title
-    plt.rc("legend", fontsize=SMALL_SIZE)  # legend fontsize
+    teach_dir = osp.expanduser("~/ASRL/temp/testing/stereo/results_run_000000")
+    repeat_dir = osp.expanduser("~/ASRL/temp/testing/stereo/results_run_000001")
 
-    # Flags
-    teach_dir = osp.expanduser("~/ASRL/temp/testing/stereo/")
-    repeat_dir = osp.expanduser("~/ASRL/temp/testing/stereo/")
-
-    r_teach = read_vo(teach_dir + "results_run_000000")
-
+    teach_vo_files = {0: "vo0_vis-exp2.csv", 1: "vo0_gps-exp2.csv"}
+    repeat_loc_files = {0: "loc1_vis-exp2.csv", 1: "loc1_gps-exp2.csv"}
+    colours = {0: ('C1', 'orange'), 1: ('C2', 'g')}
+    labels = {0: "Vision Prior", 1: "GPS Prior"}
+    r_teach = {}
     r_repeat = {}
     r_qm = {}
 
-    i = 1
-    while osp.exists(repeat_dir + "results_run_" + str(i).zfill(6)):
-        r_repeat[i], r_qm[i] = read_loc(teach_dir + "results_run_000000", repeat_dir + "results_run_" + str(i).zfill(6))
-        i = i + 1
+    for i in range(2):
+        r_teach[i] = read_vo(teach_dir, vo_file=teach_vo_files[i])
+        r_repeat[i], r_qm[i] = read_loc(teach_dir, repeat_dir, loc_file=repeat_loc_files[i], teach_vo_file=teach_vo_files[i])
 
-    print("Number of teach points: ", r_teach.shape[0])
-    print("Number of points in first repeat: ", r_repeat[1].shape[0])
+    print("Number of points in first teach: ", r_teach[0].shape[0])
+    print("Number of points in first repeat: ", r_repeat[0].shape[0])
 
     fig = plt.figure(1)
     ax = fig.add_subplot(111)
     plt.axis('equal')
-    ax.plot(r_teach[:, 0], r_teach[:, 1], label='Teach')
 
-    for j, run in r_repeat.items():
-        ax.plot(run[:, 0], run[:, 1], label='Repeat ' + str(j))
+    ax.plot(r_teach[0][:, 0], r_teach[0][:, 1], label='Teach', c='C7')
+    for i, run in r_repeat.items():
+        ax.plot(run[:, 0], run[:, 1], label='Repeat - {0}'.format(labels[i]), c=colours[i][0])
 
     plt.title('Overhead View of Integrated VO and Localization')
     ax.set_xlabel('x (m)')
     ax.set_ylabel('y (m)')
     plt.legend()
 
-    fig = plt.figure(2)
-    latest = max(r_qm, key=int)
-    plt.plot(r_qm[latest][:, 0], label='x')
-    plt.plot(r_qm[latest][:, 1], label='y')
-    plt.plot(r_qm[latest][:, 2], label='z')
-    # for j, run in r_qm.items():
-    #   plt.plot(run[:, 0], label='x_'+str(j), c='C0')      # plots all errors but messy
-    # plt.plot(run[:, 1], label='y_'+str(j), c='C1')
-    # plt.plot(run[:, 2], label='z_'+str(j), c='C2')
-    plt.title('Estimated Path-Tracking Error')
-    plt.ylabel('Distance (m)')
-    plt.legend()
+    # CALCULATE AND PLOT ERRORS OF LOCALIZATION ESTIMATES WRT GROUND TRUTH
+    fig2, ax2 = plt.subplots(nrows=2, ncols=1, figsize=[8, 6])
+    # fig3, ax3 = plt.subplots(nrows=2, ncols=1, figsize=[8, 6])
+    # fig3.subplots_adjust(left=0.10, bottom=0.06, right=0.96, top=0.93)
+    fig4 = plt.figure(4, figsize=[9, 4])
 
-    # Calculate and plot errors of localization estimates wrt ground truth
-    # Get localization estimates and times in GPS receiver frame
-    T_sv = np.eye(4)      # todo: better way to handle files
-    r_loc_in_gps_frame = read_loc_sensor(teach_dir + "results_run_000000_vis", repeat_dir + "results_run_000001_vis", T_sv)
-    r_loc_in_gps_frame2 = read_loc_sensor(teach_dir + "results_run_000000_gps", repeat_dir + "results_run_000001_gps", T_sv)
-
-    # READ GROUND TRUTH
+    # Read ground truth
     groundtruth_dir = '${VTRDATA}/june16-gt/'
     teach_gt_file = 'june16b.csv'
     repeat_gt_file = 'june16a.csv'
@@ -222,28 +210,50 @@ def main():
     gt_repeat_path = osp.join(osp.expanduser(osp.expandvars(groundtruth_dir)), repeat_gt_file)
     gt_teach = read_gpgga(gt_teach_path, 0)
     gt_repeat = read_gpgga(gt_repeat_path, 0)
+    T_sv = np.eye(4)
+    T_sv[0, 3] = -0.60
+    T_sv[1, 3] = 0.00
+    T_sv[2, 3] = -0.52
 
-    interpolate_and_rotate(gt_repeat, gt_teach, r_loc_in_gps_frame)
-    interpolate_and_rotate(gt_repeat, gt_teach, r_loc_in_gps_frame2)
+    for i in range(2):
+        # Get localization estimates and times in GPS receiver frame
+        r_loc_in_gps_frame = read_loc_sensor(teach_dir, repeat_dir, T_sv, loc_file=repeat_loc_files[i], teach_vo_file=teach_vo_files[i])
+        interpolate_and_rotate(gt_repeat, gt_teach, r_loc_in_gps_frame)
 
-    fig3, ax3 = plt.subplots(nrows=2, ncols=1, figsize=[8, 6])
-    fig3.subplots_adjust(left=0.10, bottom=0.06, right=0.96, top=0.93)
-    plt.title("Estimated and Ground Truth Path-Tracking Errors in x and y")
-    ax3[0].plot(r_loc_in_gps_frame[:, 2], c='C0', label='x - estimated')  # todo - clean up
-    ax3[0].plot(r_loc_in_gps_frame[:, 5], c='C2', label='x - gt')
-    ax3[0].plot(r_loc_in_gps_frame[:, 2] - r_loc_in_gps_frame[:, 5], c='C1', label='x - error')
-    ax3[1].plot(r_loc_in_gps_frame[:, 3], c='C0', label='y - estimated')
-    ax3[1].plot(r_loc_in_gps_frame[:, 6], c='C2', label='y - gt')
-    ax3[1].plot(r_loc_in_gps_frame[:, 3] - r_loc_in_gps_frame[:, 6], c='C1', label='y - error')
+        plt.figure(2)
+        ax2[0].plot(r_qm[i][:, 0], label='x - {0}'.format(labels[i]), c=colours[i][0])
+        ax2[1].plot(r_qm[i][:, 1], label='y - {0}'.format(labels[i]), c=colours[i][0])
+        # plt.plot(r_qm[i][:, 2], label='z - {0}'.format(labels[i]), c=colours[i][2])
+
+        # plt.figure(3)
+        # ax3[0].plot(r_loc_in_gps_frame[:, 8], r_loc_in_gps_frame[:, 2], c=colours[i][1], label='x - estimated')  # todo - fix colours
+        # ax3[0].plot(r_loc_in_gps_frame[:, 8], r_loc_in_gps_frame[:, 5], c='k', label='x - gt')
+        # ax3[0].plot(r_loc_in_gps_frame[:, 8], r_loc_in_gps_frame[:, 2] - r_loc_in_gps_frame[:, 5], c=colours[i][0], label='x - error')
+        # ax3[1].plot(r_loc_in_gps_frame[:, 8], r_loc_in_gps_frame[:, 3], c=colours[i][2], label='y - estimated')
+        # ax3[1].plot(r_loc_in_gps_frame[:, 8], r_loc_in_gps_frame[:, 6], c='k', label='y - gt')
+        # ax3[1].plot(r_loc_in_gps_frame[:, 8], r_loc_in_gps_frame[:, 3] - r_loc_in_gps_frame[:, 6], c=colours[i][2], label='y - error')
+
+        plt.figure(4)
+        plt.plot(r_loc_in_gps_frame[:, 8], r_loc_in_gps_frame[:, 3] - r_loc_in_gps_frame[:, 6], c=colours[i][0], label=labels[i])
+
+    plt.figure(2)
+    ax2[0].set_title('Estimated Path-Tracking Errors')
+    ax2[0].set_ylim([-1.2, 1.2])
+    ax2[1].set_ylim([-1.2, 1.2])
+    ax2[0].set_ylabel("Longitudinal Error (m)")
+    ax2[1].set_ylabel("Lateral Error (m)")
+    plt.xlabel('Distance Along Path (m)')
     plt.legend()
 
-    # Compares localization from two different runs
-    fig4 = plt.figure(4, figsize=[9, 4])
+    # plt.figure(3)
+    # plt.title("Estimated and Ground Truth Path-Tracking Errors in x and y")
+    # ax3[0].legend()
+    # ax3[1].legend()
+
+    plt.figure(4)
     plt.title("Localization Estimate Errors")
-    plt.plot(r_loc_in_gps_frame[:, 8], r_loc_in_gps_frame[:, 3] - r_loc_in_gps_frame[:, 6], c='C0', label='VO prior')
-    plt.plot(r_loc_in_gps_frame2[:, 8], r_loc_in_gps_frame2[:, 3] - r_loc_in_gps_frame2[:, 6], c='C2', label='GPS prior')
-    plt.ylim([-0.25, 0.25])
-    plt.ylabel("Error wrt Ground Truth (m)")
+    plt.ylim([-0.4, 0.4])
+    plt.ylabel("Lateral Error wrt Ground Truth (m)")
     plt.xlabel("Distance Along Path (m)")
     plt.legend()
 
